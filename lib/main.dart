f0d899c94d7c47e5f6a8310b2d0b844aa039c70a
berkansevimli <elektrotee12@gmail.com>
2022-03-19 19:53:37 +0300

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,145 @@
+import 'package:agora_rtc_engine/rtc_engine.dart';
+import 'package:flutter/material.dart';
+import 'package:agora_rtc_engine/rtc_local_view.dart' as RtcLocalView;
+import 'package:agora_rtc_engine/rtc_remote_view.dart' as RtcRemoteView;
+import 'package:permission_handler/permission_handler.dart';
+
+const APP_ID = "b3f9b07c88df44b985d3e780cd68710a";
+const Token =
+    "006b3f9b07c88df44b985d3e780cd68710aIABZA8Z9B4XBK+18TBTp+sDmh2cmj5cCMrTuWqpmI7NCxgYkvQsAAAAAEACNSCPiv9szYgEAAQC/2zNi";
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late RtcEngine _engine;
+  bool _joined = false;
+  int _remoteUid = 0;
+  bool _switch = false;
+
+  @override
+  void initState() {
+    super.initState();
+    initAgora();
+  }
+
+  Future<void> initAgora() async {
+    // retrieve permissions
+    await [Permission.microphone, Permission.camera].request();
+
+    //create the engine
+    _engine = await RtcEngine.createWithConfig(RtcEngineConfig(APP_ID));
+
+    await _engine.enableVideo();
+
+    _engine.setEventHandler(
+      RtcEngineEventHandler(
+        joinChannelSuccess: (String channel, int uid, int elapsed) {
+          print("local user $uid joined");
+          setState(() {
+            _joined = true;
+          });
+        },
+        userJoined: (int uid, int elapsed) {
+          print("remote user $uid joined");
+          setState(() {
+            _remoteUid = uid;
+          });
+        },
+        userOffline: (int uid, UserOfflineReason reason) {
+          print("remote user $uid left channel");
+          setState(() {
+        _remoteUid = 0;
+      });
+        },
+      ),
+    );
+
+    await _engine.joinChannel(Token, "chanel", null, 0);
+  }
+
+  // Create UI with local view and remote view
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('HanMobil Video Chat'),
+      ),
+      body: Stack(
+          children: [
+            Center(
+              child: _switch ? _renderRemoteVideo() : _renderLocalPreview(),
+            ),
+            Align(
+              alignment: Alignment.topLeft,
+              child: Container(
+                width: 100,
+                height: 100,
+                color: Colors.blue,
+                child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      _switch = !_switch;
+                    });
+                  },
+                  child: Center(
+                    child:
+                    _switch ? _renderLocalPreview() : _renderRemoteVideo(),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+    );
+  }
+
+  // Local preview
+  Widget _renderLocalPreview() {
+    if (_joined) {
+      return RtcLocalView.SurfaceView();
+    } else {
+      return Text(
+        'Please join channel first',
+        textAlign: TextAlign.center,
+      );
+    }
+  }
+
+  // Remote preview
+  Widget _renderRemoteVideo() {
+    if (_remoteUid != 0) {
+      return RtcRemoteView.SurfaceView(
+        uid: _remoteUid,
+        channelId: "chanel",
+      );
+    } else {
+      return Text(
+        'Please wait remote user join',
+        textAlign: TextAlign.center,
+      );
+    }
+  }
+}
