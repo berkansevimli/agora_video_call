b6a0ed74f88808c1f9753254c318e71ec683ca3a
berkansevimli <62310854+berkansevimli@users.noreply.github.com>
2022-03-19 19:55:39 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,8 @@ import 'package:agora_rtc_engine/rtc_local_view.dart' as RtcLocalView;
 import 'package:agora_rtc_engine/rtc_remote_view.dart' as RtcRemoteView;
 import 'package:permission_handler/permission_handler.dart';
 
-const APP_ID = "b3f9b07c88df44b985d3e780cd68710a";
-const Token =
-    "006b3f9b07c88df44b985d3e780cd68710aIABZA8Z9B4XBK+18TBTp+sDmh2cmj5cCMrTuWqpmI7NCxgYkvQsAAAAAEACNSCPiv9szYgEAAQC/2zNi";
+const APP_ID = "YOUR_APP_ID";
+const Token ="YOUR_TOKEN";
 
 void main() {
   runApp(const MyApp());
@@ -19,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Video Call App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -85,7 +84,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('HanMobil Video Chat'),
+        title: const Text('Video Chat'),
       ),
       body: Stack(
           children: [
